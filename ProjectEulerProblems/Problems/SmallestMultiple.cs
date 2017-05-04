cefd3a72ac56d305bf116544f02d3ff78941f96b
Daniel Tattan-Birch <dantb@hotmail.co.uk>
2017-05-05 00:15:14 +0100

    Skeleton for next problem

---
+++ ProjectEulerProblems/Problems/SmallestMultiple.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProjectEulerProblems.Problems
+{
+    public class SmallestMultiple : IProblem
+    {
+        public int ProblemId { get { return 5; } }
+
+        public double GetSolution()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
