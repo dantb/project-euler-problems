ba819a36b10a7de1cea408bec605a93bdfce8a3d
Daniel Tattan-Birch <dantb@hotmail.co.uk>
2017-05-04 23:26:29 +0100

    New problem added

---
+++ ProjectEulerProblems/Problems/LargestPalindromeProduct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblems
+{
+    public class LargestPalindromeProduct : IProblem
+    {
+        public int ProblemId { get { return 4; } }
+
+        public double GetSolution()
+        {
+            double largestPalindrome = 0;
+
+
+
+            return largestPalindrome;
+        }
+    }
+}
