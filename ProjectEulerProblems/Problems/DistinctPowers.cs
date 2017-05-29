5380a123dc3ed1de87f853845272e7a96df11b00
dantb <dantb@hotmail.co.uk>
2017-05-29 22:56:50 +0100

    Added dictinct powers

---
+++ ProjectEulerProblems/Problems/DistinctPowers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProjectEulerProblems
+{
+    public class DistinctPowers : IProblem
+    {
+        public int ProblemId { get { return 29; } }
+
+        public double GetSolution()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
