520744eca4aa986245217c630eff529d963c3283
dantb <dantb@hotmail.co.uk>
2017-06-09 23:57:56 +0100

    Added digit fifth powers problem

---
+++ ProjectEulerProblems/Problems/DigitFifthPowers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProjectEulerProblems
+{
+    public class DigitFifthPowers : IProblem
+    {
+        public int ProblemId { get { return 30; } }
+
+
+        public double GetSolution()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
