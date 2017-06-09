21696eeca4bd197ab82c578981d3967bf02680a6
dantb <dantb@hotmail.co.uk>
2017-06-10 00:46:15 +0100

    Add pandigital products

---
+++ ProjectEulerProblems/Problems/PandigitalProducts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProjectEulerProblems
+{
+    class PandigitalProducts : IProblem
+    {
+        public int ProblemId { get { return 32; } }
+
+        public double GetSolution()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
