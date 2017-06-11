205e8a1c8e8640360c0167a7d8b15c534049583c
dantb <dantb@hotmail.co.uk>
2017-06-11 16:53:33 +0100

    Added digit factorials problem

---
+++ ProjectEulerProblems/Problems/DigitFactorials.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProjectEulerProblems
+{
+    class DigitFactorials : IProblem
+    {
+        public int ProblemId { get { return 34; } }
+
+        public double GetSolution()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
