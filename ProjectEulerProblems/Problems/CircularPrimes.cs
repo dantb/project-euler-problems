65717ada416f129f22f179662bd434e91608eade
dantb <dantb@hotmail.co.uk>
2017-06-17 23:37:42 +0100

    Added circular primes problem

---
+++ ProjectEulerProblems/Problems/CircularPrimes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProjectEulerProblems.Problems
+{
+    public class CircularPrimes : IProblem
+    {
+        public int ProblemId
+        {
+            get { return 35; }
+        }
+
+        public double GetSolution()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
