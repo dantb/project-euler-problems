8739dd69169e4dabb61c40069f5081eaf1a28701
dantb <dantb@hotmail.co.uk>
2017-06-21 21:03:36 +0100

    Structure for problem created, next is implementation

--- ProjectEulerProblems/Problems/TruncatablePrimes.cs
+++ ProjectEulerProblems/Problems/TruncatablePrimes.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ProjectEulerProblems
 {
@@ -8,7 +10,44 @@ namespace ProjectEulerProblems
 
         public double GetSolution()
         {
-            throw new NotImplementedException();
+            TruncatablePrimeCalculator truncPrimeCalc = new TruncatablePrimeCalculator();
+            HashSet<int> truncatablePrimes = new HashSet<int>();
+            int counter = 23; //first is 23 by observation
+
+            while (truncatablePrimes.Count < 11)
+            {
+                if (truncPrimeCalc.IsTruncatablePrime(counter))
+                {
+                    truncatablePrimes.Add(counter);
+                }
+            }
+
+            return truncatablePrimes.Sum();
+        }
+
+        private bool IsPrime(int n)
+        {
+            //If there's a prime > root(n) then there's certainly one < root(n)
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            if (n == 1)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        class TruncatablePrimeCalculator
+        {
+            public bool IsTruncatablePrime(int counter)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
